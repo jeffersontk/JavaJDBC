f0f930884b0fc5cb19ec26a2b40374947259c71c
0rakul0 <0rakul0render@gmail.com>
2020-10-11 20:05:15 -0300

    rename and Create
    pagina para criação de novas linhas e uma para vizualizar

---
+++ src/app/Create.java
@@ -0,0 +1,44 @@
+package app;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import db.DB;
+
+public class Create {
+ public static void main(String[] args) {
+	 //abrir conexão
+	 Connection conn = null;
+	 //prepara o statemente
+	 PreparedStatement st = null;
+	 
+	 try {
+		 conn = DB.getConnection();
+// inserir cod sql aqui na tabela desejada
+// lembrando dos nomes das colunas
+		 st = conn.prepareStatement(
+				 "INSERT INTO times" +
+				 "(nome, posicao)" +
+				 
+				 "VALUES " +
+				 "(?,?)"
+				 );
+//a virgula é o que será substituido por informações
+//logo esse é um codigo generico para a minha tabala só com duas colunas.
+		 	st.setString(1, "uniCarioca");
+			st.setInt(2, 4);
+//não precisa de aspas se for numeros
+//caso precise de data ==> 
+//st.setDate(posicaoColuna, new java.sql.Date(sdf.parse("dd/mm/aaaa").getTime()));
+			
+			// o cara que faz as coisas acontecerem é esse ai hó
+			int rowsAffected = st.executeUpdate();
+			
+			System.out.println("Linhas afetadas: "+ rowsAffected);
+	 }
+	 catch (SQLException e) {
+		e.printStackTrace();
+	}
+}
+}
--- src/app/View.java
+++ src/app/View.java
@@ -7,7 +7,7 @@ import java.sql.Statement;
 
 import db.DB;
 
-public class Program {
+public class View {
 
 	public static void main(String[] args) {
 		
